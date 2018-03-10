80a16e73d62bc06417a228677c698592f690ec51
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-03-10 17:43:02 +0100

    s

--- PostEmailsEncriptados/Controllers/HomeController.cs
+++ PostEmailsEncriptados/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using PostEmailsEncriptados.Models;
 
 namespace PostEmailsEncriptados.Controllers
 {
@@ -26,5 +28,53 @@ namespace PostEmailsEncriptados.Controllers
 
             return View();
         }
+        [HttpGet]
+        public ActionResult EnviarCorreo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EnviarCorreo(String para, String asunto, String mensaje
+            , HttpPostedFileBase fichero)
+        {
+            try
+            {
+                MailMessage correo = new MailMessage();
+                correo.From = new MailAddress("correosencriptados123@gmail.com", "JJ");
+                correo.To.Add(para);
+                correo.Subject = asunto;
+                correo.Body = Cipher.Encrypt(mensaje, "123");
+                correo.IsBodyHtml = true;
+                correo.Priority = MailPriority.Normal;
+                 
+
+                //String ruta = Server.MapPath("../Temporal");
+                //fichero.SaveAs(ruta + "\\" + fichero.FileName);
+
+                //Attachment adjunto = new Attachment(ruta + "\\" + fichero.FileName);
+                //correo.Attachments.Add(adjunto);
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 25;
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+                string sCuentaCorreo = "correosencriptados123@gmail.com";
+                string sPasswordCorreo = "tajamar365";
+                smtp.Credentials = new System.Net.NetworkCredential(sCuentaCorreo, sPasswordCorreo);
+
+                smtp.Send(correo);
+
+                ViewBag.Mensaje = "Mensaje enviado correctamente";
+                //System.IO.File.Delete(ruta + "\\" + fichero.FileName);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View();
+        }
+
     }
 }
